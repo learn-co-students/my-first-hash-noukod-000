3229c0439161e197227b6f2f9ddd86ab1732acf2
Saint-Jean Germila <germila@noukod.com>
2018-10-02 15:34:16 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-your_hash = {name : "Germie"}
+your_hash = {"name"= "Germie", "month"=> "april"}
  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 end
 
